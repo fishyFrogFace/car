c7ea99a9f9c9255a1315d6a10c454aa14027877e
Camilla Rygh <camillahallo@mail.com>
2019-01-19 11:15:34 +0100

    Remove hardcoded filename

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,16 @@ import Prelude hiding (readFile, lines)
 import Lib
 import Data.ByteString.Lazy (ByteString(..), readFile)
 import Data.ByteString.Lazy.Char8 (lines)
+import System.Environment (getArgs)
+
+process :: String -> IO ()
+process filename = do
+  content <- readFile filename
+  print $ map calcTotal $ lines content
 
 main :: IO ()
-main = do 
-       content <- readFile "src/content.txt"
-       print $ map calcTotal $ lines content
+main = do
+  args <- getArgs
+  case args of
+    [x] -> process x
+    _   -> putStrLn "You need to provide a filename"
